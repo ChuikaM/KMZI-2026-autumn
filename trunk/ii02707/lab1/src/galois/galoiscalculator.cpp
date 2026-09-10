ce253ba50a9b057c6ec879d387d2d87c369271ac
ChuikaM <matveidoroshenko@gmail.com>
2026-09-11 01:20:14 +0300

    feat: add add & multiply methods.

--- trunk/ii02707/lab1/src/galois/galoiscalculator.cpp
+++ trunk/ii02707/lab1/src/galois/galoiscalculator.cpp
@@ -1,8 +1,32 @@
-#include "galoiscalculator.hpp"
+#include <galoiscalculator.hpp>
 
 GALOIS_INT galois::GaloisCalculator::add(GALOIS_INT a, GALOIS_INT b)
 {
+    return static_cast<GALOIS_INT>(a ^ b);
+}
+
+GALOIS_INT galois::GaloisCalculator::multiply(GALOIS_INT a, GALOIS_INT b, GALOIS_INT p_x)
+{
+    GALOIS_INT P_x = 0;
     for(auto i = 0; i < FIELD_POWER; i++) {
-        
+        if(b & (1 << i)) {
+            P_x ^= (a << i);
+        }
     }
+
+    const auto reduce = [](GALOIS_INT P_x, GALOIS_INT p_x) -> GALOIS_INT {
+        // When FIELD_POWER = 8 -> 2*FIELD_POWER-1 = 14 
+        for(int i = 2*FIELD_POWER-2; i >= FIELD_POWER; i--) { 
+            if(P_x & (1 << i)) {
+                P_x ^= (p_x << (i - FIELD_POWER));
+            }
+        }
+        return P_x;
+    };
+    return reduce(P_x, p_x);
+}
+
+GALOIS_INT galois::GaloisCalculator::inverse(GALOIS_INT a, GALOIS_INT p_x)
+{
+    return GALOIS_INT();
 }
--- trunk/ii02707/lab1/src/galois/galoiscalculator.hpp
+++ trunk/ii02707/lab1/src/galois/galoiscalculator.hpp
@@ -1,15 +1,43 @@
 #pragma once
 #include <string>
 #include <algorithm>
+#include <sstream>
 
-typedef uint8_t GALOIS_INT;
+typedef int GALOIS_INT;
 typedef std::string GALOIS_FIELD;
 
 namespace galois {
 
 // x^8+x^5+x^2+x+1 (0x12B)
-GALOIS_INT galois_field_to_int(GALOIS_FIELD field) {
+int galois_field_to_int(const std::string& poly) {
+    int result = 0;
     
+    std::stringstream ss(poly);
+    std::string term;
+    
+    while (std::getline(ss, term, '+')) {
+        size_t caretPos = term.find('^');
+        
+        if (caretPos != std::string::npos) {
+            std::string expStr = term.substr(caretPos + 1);
+            std::stringstream expSS(expStr);
+            int exponent;
+            expSS >> exponent;
+            
+            result += (1 << exponent);
+        } else {
+            if (term.find('x') != std::string::npos) {
+                result += (1 << 1);
+            } else {
+                std::stringstream constSS(term);
+                int constant;
+                constSS >> constant;
+                result += constant;
+            }
+        }
+    }
+    
+    return result;
 }
 
 // p(x) = x^128+x^7+x^2+x+1
